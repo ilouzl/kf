6e5a7ad6f60de3e1fe84ec0bca203ad26eee6683
Liron Ilouz <Liron@lirons-MacBook-Pro.local>
2016-03-09 22:06:20 +0200

    Constant measurement bias estimation. Measurement matrix is modified to increase convergence speed

--- kf_test_script.m
+++ kf_test_script.m
@@ -6,25 +6,27 @@ Fs = 100; T = 5; t = 0:1/Fs:T;
 
 % Model - x[k] = F*x[k-1]+B*u[k]+W*w[n]
 %         w[n] ~ N(0,Q = W*cov(w[n])*W')
-F = [1 1/Fs; 0 1]; 
-B = [((1/Fs)^2)/2 ; 1/Fs]; 
+F = [1 1/Fs 0; ...
+     0 1    0; ...
+     0 0    1]; 
+B = [((1/Fs)^2)/2 ; 1/Fs; 0]; 
 u = 2; 
-W = 0; 
+W = [1;1;0]; 
 sigma_w = 0;
 Q = W*W'*sigma_w^2;
 
 % Observation - z[k] = H*x[k]+V*v[n]
 %               v[n] ~ N(0,R = V*cov(v[n])*V')
-H = [1 0]; 
+H = [1 0 100]; 
 V = 1; 
-sigma_v = 4;
+sigma_v = 1;
 R = V*V'*sigma_v^2;
 
 
 % initialization
-x = [0;0];         % initial state
-x11 = [1;1];           % assume we know the initial position
-P11 = 5*eye(2);  % assume initial estimation is correct
+x = [0;0;0.3];         % initial state
+x11 = [0;0;0];           % assume we know the initial position
+P11 = diag([1,1,1]);  % assume initial estimation is correct
 filt = []; true = []; meas = []; P = [];
 for i=1:length(t)
     x_1_1 = x11; P_1_1 = P11;
@@ -39,7 +41,8 @@ for i=1:length(t)
 end
 subplot(3,1,1);
 plot(t,true(1,:),t,meas(1,:),t,filt(1,:)); 
+legend({'true','meas','est'});
 subplot(3,1,2);
 plot(t,true(2,:),t,filt(2,:)); 
 subplot(3,1,3);
-plot(t,P(1,:)); 
+plot(t,filt(3,:)); 
